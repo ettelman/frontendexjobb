1ed6cb70ba465f06cc380bcbbf55d46803aef38f
Björn.Ettelman <bjet2100@student.miun.se>
2023-06-01 11:41:29 +0200

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <template>
+ <div v-if="showCookieMessage" id="cookieMessage" class="cookie-banner">
+    <div class="cookie-content">
+      <p>Vi använder cookies på vår webbplats för att förbättra din användarupplevelse och hålla dig inloggad mellan olika webbplatser. </p>
+      <button @click="acceptCookies">Godkänn</button>
+    </div>
+  </div>
   <div id="app">
     <div class="top">
       <img src="./assets/bonnier.png" alt="Logotyp för Bonnier News" class="logo"/>
-      <a href="#" class="nav-link mobile-offset" @click="toggleTheme"><i class="pi pi-tag"></i>Ljus/mörkt tema</a>
+      <a href="#" class="nav-link mobile-offset" @click="toggleTheme"><i class="pi pi-tag"></i>Ljust/mörkt tema</a>
     </div>
     <div class="container">
-    <router-view></router-view>
-    
-  </div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import { RouterView } from 'vue-router';
+import { RouterView } from "vue-router";
+import { ref } from "vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     RouterView,
   },
-  methods: {
-    toggleTheme() {
-      document.body.classList.toggle('dark');
-    },
+  setup() {
+    const showCookieMessage = ref(!localStorage.getItem("cookiesAccepted"));
+
+    const acceptCookies = () => {
+      localStorage.setItem("cookiesAccepted", "true");
+      showCookieMessage.value = false;
+    }
+
+    const toggleTheme = () => {
+      document.body.classList.toggle("dark");
+    }
+
+    return { showCookieMessage, acceptCookies, toggleTheme };
   },
 };
 </script>
+
 
 <style>
 #app {
@@ -37,10 +53,31 @@ export default {
 .logo {
   display: block;
 }
+.cookie-banner {
+  position: fixed;
+  bottom: 0;
+  max-width: 1000px;
+  min-width: 50%;
+  background-color: rgba(0,0,0,0.9);
+  z-index: 10000;
+}
+.cookie-content {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+.cookie-banner p {
+  display: inline;
+}
+.cookie-banner button {
+  
+  padding: 5px 10px;
+}
 @media (max-width: 900px) {
 #app {
   padding: 0.1rem;
 }
 }
-
 </style>
